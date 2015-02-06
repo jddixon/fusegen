3a2f8749196186ca5f73d422c0d61c5b5a9261df
Jim Dixon <jddixon@gmail.com>
2015-02-06 13:49:20 -0800

    proj: generate fsync, readlink, ftruncate

--- fusegen/__init__.py
+++ fusegen/__init__.py
@@ -13,8 +13,8 @@ __all__ = [ '__version__', '__version_date__',
        ]
 
 # -- exported constants ---------------------------------------------
-__version__      = '0.3.8'
-__version_date__ = '2015-02-05'
+__version__      = '0.3.9'
+__version_date__ = '2015-02-06'
 
 # path to text file of quasi-prototypes
 PATH_TO_FIRST_LINES = 'fragments/prototypes'
