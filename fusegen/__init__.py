c6e1d103adac5bb70d417743e09e0f6364eb297f
Jim Dixon <jddixon@gmail.com>
2015-02-09 08:20:25 -0800

    proj: generates readdir

--- fusegen/__init__.py
+++ fusegen/__init__.py
@@ -13,8 +13,8 @@ __all__ = [ '__version__', '__version_date__',
        ]
 
 # -- exported constants ---------------------------------------------
-__version__      = '0.3.11'
-__version_date__ = '2015-02-08'
+__version__      = '0.3.12'
+__version_date__ = '2015-02-09'
 
 # path to text file of quasi-prototypes
 PATH_TO_FIRST_LINES = 'fragments/prototypes'
