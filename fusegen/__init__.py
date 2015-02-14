6127015ebd6f373fd032ee2de2e606321e0e8f37
Jim Dixon <jddixon@gmail.com>
2015-02-14 14:24:47 -0800

    proj: add config.{guess,sub}, automate copying to distro

--- fusegen/__init__.py
+++ fusegen/__init__.py
@@ -13,8 +13,8 @@ __all__ = [ '__version__', '__version_date__',
        ]
 
 # -- exported constants ---------------------------------------------
-__version__      = '0.4.2'
-__version_date__ = '2015-02-13'
+__version__      = '0.4.3'
+__version_date__ = '2015-02-14'
 
 # path to text file of quasi-prototypes
 PATH_TO_FIRST_LINES = 'fragments/prototypes'
