98fd527479092f8a0124a7e834289440476aa195
Jim Dixon <jddixon@gmail.com>
2015-02-15 17:20:26 -0800

    proj: generate header files, mount/umount code

--- fusegen/__init__.py
+++ fusegen/__init__.py
@@ -13,8 +13,8 @@ __all__ = [ '__version__', '__version_date__',
        ]
 
 # -- exported constants ---------------------------------------------
-__version__      = '0.4.3'
-__version_date__ = '2015-02-14'
+__version__      = '0.4.4'
+__version_date__ = '2015-02-15'
 
 # path to text file of quasi-prototypes
 PATH_TO_FIRST_LINES = 'fragments/prototypes'
