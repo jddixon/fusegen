978a54e88b87d5f866822f957232a3026d34b16c
Jim Dixon <jddixon@gmail.com>
2015-02-17 12:19:03 -0800

    proj: generate main.inc plus many small fixes

--- fusegen/__init__.py
+++ fusegen/__init__.py
@@ -13,8 +13,8 @@ __all__ = [ '__version__', '__version_date__',
        ]
 
 # -- exported constants ---------------------------------------------
-__version__      = '0.4.5'
-__version_date__ = '2015-02-16'
+__version__      = '0.4.6'
+__version_date__ = '2015-02-17'
 
 # path to text file of quasi-prototypes
 PATH_TO_FIRST_LINES = 'fragments/prototypes'
