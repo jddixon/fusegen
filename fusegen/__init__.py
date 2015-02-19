78531637500c40196ad450bb632bedb0161a9968
Jim Dixon <jddixon@gmail.com>
2015-02-19 12:28:06 -0800

    proj: many minor fixes, some cosmetic

--- fusegen/__init__.py
+++ fusegen/__init__.py
@@ -13,8 +13,8 @@ __all__ = [ '__version__', '__version_date__',
        ]
 
 # -- exported constants ---------------------------------------------
-__version__      = '0.4.7'
-__version_date__ = '2015-02-18'
+__version__      = '0.4.8'
+__version_date__ = '2015-02-19'
 
 # path to text file of quasi-prototypes
 PATH_TO_FIRST_LINES = 'fragments/prototypes'
