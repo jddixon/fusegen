433877dfb8b323527a3907c90700470d2075c2d0
Jim Dixon <jddixon@gmail.com>
2015-02-22 16:43:55 -0800

    proj: improved checks on CLI arg list

--- fusegen/__init__.py
+++ fusegen/__init__.py
@@ -13,8 +13,8 @@ __all__ = [ '__version__', '__version_date__',
        ]
 
 # -- exported constants ---------------------------------------------
-__version__      = '0.4.10'
-__version_date__ = '2015-02-21'
+__version__      = '0.4.11'
+__version_date__ = '2015-02-22'
 
 # path to text file of quasi-prototypes
 PATH_TO_FIRST_LINES = 'fragments/prototypes'
