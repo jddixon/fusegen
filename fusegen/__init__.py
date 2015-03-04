bdfb1c0e35a67145b7c62c3f8a9561155bbef41c
Jim Dixon <jddixon@gmail.com>
2015-03-04 12:47:43 -0800

    proj: add build, writeBucket(), misc fixes

--- fusegen/__init__.py
+++ fusegen/__init__.py
@@ -13,8 +13,8 @@ __all__ = [ '__version__', '__version_date__',
        ]
 
 # -- exported constants ---------------------------------------------
-__version__      = '0.5.6'
-__version_date__ = '2015-03-03'
+__version__      = '0.5.7'
+__version_date__ = '2015-03-04'
 
 # path to text file of quasi-prototypes
 PATH_TO_FIRST_LINES = 'fragments/prototypes'
