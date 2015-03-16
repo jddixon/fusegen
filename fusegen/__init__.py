03d9dacb2e526a2149cc3a960408143fbc3299f1
Jim Dixon <jddixon@gmail.com>
2015-03-16 14:48:45 -0700

    proj: set maxRead/Write to 128KB, prevent seg faults by dynamically allocating op table

--- fusegen/__init__.py
+++ fusegen/__init__.py
@@ -13,8 +13,8 @@ __all__ = [ '__version__', '__version_date__',
        ]
 
 # -- exported constants ---------------------------------------------
-__version__      = '0.6.7'
-__version_date__ = '2015-03-15'
+__version__      = '0.6.8'
+__version_date__ = '2015-03-16'
 
 # path to text file of quasi-prototypes
 PATH_TO_FIRST_LINES = 'fragments/prototypes'
